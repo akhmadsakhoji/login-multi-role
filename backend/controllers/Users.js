c06fe4c3a275428b2173bd7ed9135d305e9987d7
Akhmad Sakhoji Jamaludin <akhmadsakhoji@gmail.com>
2023-01-03 01:30:50 +0700

    melengkapi controller users tahap 1

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,13 +1,44 @@
 import User from "../models/UserModel.js";
+import argon2, { hash } from "argon2";
 
-export const getUsers = (req, res) => {
-    
+export const getUsers = async(req, res) => {
+    try {
+        const response = await User.findAll({
+            attributes:['uuid', 'name', 'email', 'role']
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
-export const getUserById = (req, res) => {
-
+export const getUserById = async(req, res) => {
+    try {
+        const response = await User.findOne({
+            attributes:['uuid', 'name', 'email', 'role'],
+            where:{
+                uuid: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
-export const createUser = (req, res) => {
-
+export const createUser = async(req, res) => {
+    const {name, email, password, confPassword, role} = req.body;
+    if(password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    const hashPassword = await argon2.hash(password);
+    try {
+        await User.create({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        })
+        res.status(201).json({msg: "Register Berhasil"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
 export const updateUser = (req, res) => {
 
